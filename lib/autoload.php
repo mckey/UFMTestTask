a418ceb1b739f9efe37fed4cf676e426b9a7483e
Vladimir Makarov <mckey11@gmail.com>
2021-08-31 17:03:48 +0300

    Prettifying autoload

--- lib/autoload.php
+++ lib/autoload.php
@@ -1,13 +1,11 @@
 <?php
 
-if (!class_exists('Ifm\Ifm', false)) {
-    spl_autoload_register(
-        function ($class_name) {
-            $path = __DIR__ . "/models/$class_name.class.php";
+spl_autoload_register(
+    function ($class_name) {
+        $path = __DIR__ . "/models/$class_name.class.php";
 
-            if (file_exists($path)) {
-                require_once $path;
-            }
+        if (file_exists($path)) {
+            require_once $path;
         }
-    );
-}
+    }
+);
--- lib/models/ApplicationRouter.class.php
+++ lib/models/ApplicationRouter.class.php
@@ -5,10 +5,10 @@ class ApplicationRouter
     public static function forwardTo(string $application, string $action)
     {
         $application_class = ucfirst($application) . 'Application';
-        $application_class_file =  __DIR__ . '/../../app/' . $application_class . '.class.php';
+        $application_class_file =  __DIR__ . "/../../app/$application_class.class.php";
         
         if (!file_exists($application_class_file)) {
-            throw new \InvalidArgumentException(sprintf('The application "%s" does not exist.', $application_class_file));
+            throw new InvalidArgumentException(sprintf('The application "%s" does not exist.', $application));
         }
 
         require_once $application_class_file;
